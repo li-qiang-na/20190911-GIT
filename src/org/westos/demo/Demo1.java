d046ec49665a83cca6b87581cb95418451e60e5a
LQN <420630399@qq.com>
2019-09-11 17:14:37 +0800

    Initial commit,第一个

---
+++ src/org/westos/demo/Demo1.java
@@ -0,0 +1,12 @@
+package org.westos.demo;
+
+/**
+ * @description:git
+ * @author: @李小白
+ * @create: 2019-09-11 17:11
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("这是第一个git仓库的数据");
+    }
+}
