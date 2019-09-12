f7c07891507e3836955e80fc132bf027c47e8c07
LQN <420630399@qq.com>
2019-09-12 11:52:43 +0800

    这个是我的新的分支张

--- src/org/westos/demo/Demo1.java
+++ src/org/westos/demo/Demo1.java
@@ -14,6 +14,7 @@ public class Demo1 {
         System.out.println("大家好,我是小袁,我已经从仓库上或取出来了");
         System.out.println("我已经修改了部分代码已经提交到仓库了");
         System.out.println("我是小张,我已经从远程仓库上获取到小袁提交的代码了");
+        System.out.println("新的分支");
 
     }
 }
