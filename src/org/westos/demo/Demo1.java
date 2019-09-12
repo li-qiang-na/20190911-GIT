60c12131fe1a2237a424e4e2298544f116ef7caf
LQN <420630399@qq.com>
2019-09-12 11:48:35 +0800

    这个是我的新的分支张

--- src/org/westos/demo/Demo1.java
+++ src/org/westos/demo/Demo1.java
@@ -11,5 +11,9 @@ public class Demo1 {
         System.out.println("有改变");
         System.out.println("这是一个git");
         System.out.println("这是今天早上的一个数据");
+        System.out.println("大家好,我是小袁,我已经从仓库上或取出来了");
+        System.out.println("我已经修改了部分代码已经提交到仓库了");
+        System.out.println("我是小张,我已经从远程仓库上获取到小袁提交的代码了");
+
     }
 }
